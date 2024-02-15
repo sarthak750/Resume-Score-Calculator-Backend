d483f624f4edddacedec4467a893e93948b792ee
sarthak750 <sarthakvasan1234@gmail.com>
2024-02-15 16:50:46 +0530

    Updated indeedData

--- jobData/indeedData.js
+++ jobData/indeedData.js
@@ -5,7 +5,7 @@ puppeteer.use(stealthPlugin());
 const indeedData = async (url) => {
   try {
     console.log(1);
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ timeout: 60000 });
     const page = await browser.newPage();
     await page.goto(url);
 
